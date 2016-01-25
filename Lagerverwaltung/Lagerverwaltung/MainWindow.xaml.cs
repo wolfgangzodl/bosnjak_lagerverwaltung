bfc434fe5c3b20ed3f1ed931b994f31e736b2a58
wolfgangzodl <zodlwolfgang@gmail.com>
2016-01-25 18:30:20 +0100

    Initialize

---
+++ Lagerverwaltung/Lagerverwaltung/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lagerverwaltung
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+         public MainWindow()
+         {
+             InitializeComponent();
+         }
+
+        private void TextBox_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+            DateTime today = DateTime.Now;
+            TextBox textBox = sender as TextBox;
+            //Auftragsnummer.Text = today
+
+    }
+}
